b4b3fa3a44e3985084e5d7033871b684ce778400
Suhrob Ulmasov <suhrob.ulmasov.ny@gmail.com>
2021-05-04 17:14:45 -0500

    Added one work after publishing to github

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 
         System.out.println("Hello world");
         System.out.println("Adding GitHub Now");
+        System.out.println("YAY STUFFS ARE ON GITHUB!!!");
     }
 }
